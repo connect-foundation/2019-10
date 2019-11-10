ec824442a6dd1525a8945a5c9723519544d6afcf
alexKwonIsAwesome <khwalex@gmail.com>
2019-11-10 22:54:16 +0900

    [client] | feat(): material-ui 설치 및 브라우저 기본 스타일 제거
    css 라이브러리인 material-ui 라이브러리를 설치하고, 해당 라이브러리의 스타일링을 서버사이드에서 적용할 수 있도록 작성했습니다. 또, CssBaseline 컴포넌트를 활용하여, 브라우저 기본 스타일을 제거합니다.
    
    [참조]
    - https://material-ui.com/guides/server-rendering/#server-rendering
    - https://github.com/zeit/next.js/blob/canary/examples/with-material-ui/README.md

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -1,23 +1,35 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet as StyledComponentsSheet } from 'styled-components';
+import { ServerStyleSheets as MaterialSheet } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class MyDocument extends Document {
   public static async getInitialProps(ctx) {
     const styledComponentsSheet = new StyledComponentsSheet();
+    const materialSheet = new MaterialSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: App => props =>
-            styledComponentsSheet.collectStyles(<App {...props} />),
+            materialSheet.collect(
+              styledComponentsSheet.collectStyles(
+                <>
+                  <CssBaseline />
+                  <App {...props} />
+                </>,
+              ),
+            ),
         });
+
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
+            {materialSheet.getStyleElement()}
             {styledComponentsSheet.getStyleElement()}
           </>
         ),
