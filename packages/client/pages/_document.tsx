123bc86c8168f9f6595f4e8cd8c1f694c034c258
alexKwonIsAwesome <khwalex@gmail.com>
2019-11-10 22:27:09 +0900

    [client] | feat(): styled-components 추가
    styled-components 라이브러리를 nextjs에서 사용할 수 있도록 추가했습니다.

---
+++ packages/client/pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet as StyledComponentsSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  public static async getInitialProps(ctx) {
+    const styledComponentsSheet = new StyledComponentsSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentsSheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styledComponentsSheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      styledComponentsSheet.seal();
+    }
+  }
+
+  public render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const Home = () => <div>Hello world</div>;
+import styled from 'styled-components';
+
+const Home = () => (
+  <div>
+    Hello world
+    <Kang>강과눈</Kang>
+  </div>
+);
+
+const Kang = styled.div`
+  background: red;
+  color: white;
+`;
 
 export default Home;
