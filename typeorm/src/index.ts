e9e2186829660cb1f0bd204859274c974a8564ba
Atercatus <myleelee2053@naver.com>
2019-11-12 11:35:07 +0900

    [server & client] | refactor(database): 코드리뷰 시 발생한 이슈 해결

--- typeorm/src/index.ts
+++ typeorm/src/index.ts
@@ -1,21 +1,6 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection => {
-
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+createConnection()
+  .then(async connection => {})
+  .catch(error => console.log(error));
