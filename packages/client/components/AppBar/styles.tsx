ac06296054031ebde624d722bb96ff92bb1a4a37
Alex Kwon <khwalex@gmail.com>
2019-11-12 20:20:35 +0900

    [client] feat(main): 메인 페이지 frame 작성
    상단의 AppBar와 그 아래의 컨텐츠 영역을 작성했습니다.

---
+++ packages/client/components/AppBar/styles.tsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+import MaterialAppBar from '@material-ui/core/AppBar';
+import { BREAKPOINT } from '../../constants';
+
+export const AppBar = styled(MaterialAppBar)`
+  && {
+    background-color: #383d3f;
+    color: white;
+    position: fixed;
+    z-index: 2;
+  }
+`;
+
+export const Container = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  height: 6.4rem;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.div`
+  font-size: 2.8rem;
+
+  svg {
+    vertical-align: middle;
+  }
+`;
+
+export const MobileButtons = styled.div`
+  button {
+    padding: 0;
+    margin-left: 1.5rem;
+    color: white;
+    background: none;
+    border: none;
+    margin-left: 1.5rem;
+  }
+
+  @media only screen and (min-width: ${BREAKPOINT}px) {
+    display: none;
+  }
+`;
+
+export const DesktopButtons = styled.div`
+  display: none;
+
+  @media only screen and (min-width: ${BREAKPOINT}px) {
+    display: block;
+  }
+
+  a {
+    button {
+      height: 3.2rem;
+      padding: 0 1.6rem;
+      border: 0;
+      border-radius: 0.5rem;
+      background: #484c50;
+      color: white;
+      font-size: 1.6rem;
+      font-weight: bold;
+      margin-left: 1rem;
+
+      svg {
+        vertical-align: middle;
+        display: inline-block;
+        margin-right: 1rem;
+      }
+
+      span {
+        vertical-align: middle;
+      }
+    }
+
+    button.primary {
+      background-color: #02cf5d;
+    }
+  }
+`;
---
+++ packages/client/views/Main/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import AppBar from '../../components/AppBar';
+
+const Main = ({ ua }) => {
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <AppBar />
+      <main
+        style={{
+          flexGrow: 1,
+        }}
+      >
+        <div>
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+        </div>
+        <div>
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+        </div>
+        <div>
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+          eget arcu dictum varius duis at consectetur lorem. Velit sed ullamco
+        </div>
+      </main>
+    </div>
+  );
+};
+
+
+export default Main;
