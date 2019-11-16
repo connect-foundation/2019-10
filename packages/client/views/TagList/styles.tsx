9624eeb9328e0ff0170880c3f1ce1cf08482173c
Atercatus <myleelee2053@naver.com>
2019-11-16 21:31:42 +0900

    [client] | feat(TagList): 인기 태그 화면 구성
    Material-UI `Grid` component 를 이용하여 TagList component를 구성했습니다.
    Desktop 기준에서 Grid 에서 총 12를 기준으로 각각 좌우로 2의 여백이 존재합니다.
    이를 위해 최상단 Grid container에 중앙정렬로 8의 너비를 가지는 Grid item을 배치하고 그 안에 Desktop 기준 각각의 Row들을 하나의 Grid Container로 감싸 내부에 4개의 Grid item을 적재했습니다.
    이 때, Mobile에서는 2개의 Row가 하나의 Grid container가 되므로 그에 따른 Tag들의 간격을 css에서 수정해줬습니다.

---
+++ packages/client/views/TagList/styles.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import MaterialGrid from '@material-ui/core/Grid';
+
+import { BREAKPOINT, fontWeight } from '../../constants';
+
+/* window 의 넓이에 따라 변화하는 spacing을 정의해주기 위해 사용됨*/
+export const ContainerGrid = styled(MaterialGrid)`
+  && {
+    margin-bottom: 0.8rem;
+    @media only screen and (min-width: ${BREAKPOINT}px) {
+      width: calc(100% + 24px);
+      margin: -12px;
+      margin-bottom: 1.2rem;
+    }
+  }
+
+  && > .MuiGrid-item {
+    @media only screen and (min-width: ${BREAKPOINT}px) {
+      padding: 12px;
+    }
+  }
+`;
+
+export const TagList = styled.div`
+  margin: 2rem 2rem 0 2rem;
+
+  @media only screen and (min-width: ${BREAKPOINT}px) {
+    margin: 5rem 3rem 0 3rem;
+  }
+`;
+
+export const Tag = styled.div`
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #484c50;
+  border-radius: 0.5rem;
+`;
+
+export const TagTitle = styled.div`
+  font-size: 1.8rem;
+  font-weight: ${fontWeight.bold};
+  margin-bottom: 0.5rem;
+`;
+
+export const TagCount = styled.div`
+  font-size: 1.6rem;
+  font-weight: ${fontWeight.regular};
+`;
