0237db08a53967fef7f2a68335ed2bc8244e94c2
Alex Kwon <khwalex@gmail.com>
2019-11-17 18:41:26 +0900

    [client] | feat(video): 비디오 상세 페이지 구성
    비디오를 시청하는 상세 페이지의 라우트를 작성하고, 컴포넌트를 작성했습니다.

---
+++ packages/client/views/Video/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import * as S from './styles';
+
+import Layout from '../../components/Layout';
+const Video = () => {
+  return (
+    <Layout drawer={false}>
+    </Layout>
+  );
+};
+
+export default Video;
