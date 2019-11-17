88a192a28fa972ee577afcfd1bac415cd27f3457
kimjihye <eoeogudgud@naver.com>
2019-11-17 16:32:03 +0900

    [client] | feat(ArrawBackSVG): 뒤로가기 SVG 추가
    모바일 검색 활성화시 뒤로가기 작업의 SVG를 추가하였습니다.

---
+++ packages/client/svgs/ArrawBackSVG/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArrawBackSVG = props => (
+  <svg width={24} height={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+      d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"
+      fill="#fff"
+    />
+  </svg>
+);
+
+export default ArrawBackSVG;
