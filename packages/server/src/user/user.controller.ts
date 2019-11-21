920d7a9f62df735f2088c2cb4644fb3ec733cc7a
riverandeye <riverandeye@gmail.com>
2019-11-21 16:14:49 +0900

    [server] | feat(user): User api 추가
    User api를 추가하였습니다.
    Post /users = 사용자 추가
    Delete /users/:id = id에 해당하는 사용자 제거
    Put /users/:id = id에 해당하는 사용자 정보 변경

--- packages/server/src/user/user.controller.ts
+++ packages/server/src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Body,
+  Put,
+} from '@nestjs/common';
 import { User } from '../../../typeorm/src/entity/user.entity';
 import { UserService } from './user.service';
 import { Video } from '../../../typeorm/src/entity/video.entity';
+import { UserDto, UserResponseDto } from './dto';
 
 @Controller('users')
 export class UserController {
@@ -14,5 +23,23 @@ export class UserController {
   @Get(':userId/videos')
   public async getVideos(@Param('userId') userId: number): Promise<Video[]> {
     return await this.userService.findVideos(userId);
+  }
+
+  @Post('')
+  public async createUser(@Body() userDto: UserDto): Promise<UserResponseDto> {
+    return await this.userService.insertUser(userDto);
+  }
+
+  @Delete(':id')
+  public async deleteUser(@Param('id') id: number): Promise<UserResponseDto> {
+    return await this.userService.deleteUser(id);
+  }
+
+  @Put(':id')
+  public async updateUser(
+    @Param('id') id,
+    @Body() userDto: UserDto,
+  ): Promise<UserResponseDto> {
+    return await this.userService.putUser(id, userDto);
   }
 }
