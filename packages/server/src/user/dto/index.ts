ab3b00f6222c88441634c29afb2c431962f7ac65
riverandeye <riverandeye@gmail.com>
2019-11-21 15:47:45 +0900

    [server] | feat(user): User dto 정의
    User dto 및 UserResponseDto 를 정의하였습니다.

---
+++ packages/server/src/user/dto/index.ts
@@ -0,0 +1,4 @@
+import UserDto from './user.dto';
+import UserResponseDto from './userResponse.dto';
+
+export { UserDto, UserResponseDto };
---
+++ packages/server/src/user/dto/userResponse.dto.ts
@@ -0,0 +1,8 @@
+export default class UserResponseDto {
+  public readonly id: number;
+  public readonly username: string;
+  public readonly description: string;
+  public readonly avatar: string;
+  public readonly email: string;
+  public readonly githubId: string;
+}
