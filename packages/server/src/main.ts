118a61dad4b2c35553c0beba05f9dc25e3dc7e66
Atercatus <myleelee2053@naver.com>
2019-11-21 16:40:48 +0900

    [server] | feat(cors): 서버 보안 및 CORS 설정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
