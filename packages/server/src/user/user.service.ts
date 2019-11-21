4a7df508c947a82240fb580ac291e04014d0cfcf
riverandeye <riverandeye@gmail.com>
2019-11-21 16:14:49 +0900

    [server] | feat(user): User api 추가
    User api를 추가하였습니다.
    Post /users = 사용자 추가
    Delete /users/:id = id에 해당하는 사용자 제거
    Put /users/:id = id에 해당하는 사용자 정보 변경

--- packages/server/src/user/user.service.ts
+++ packages/server/src/user/user.service.ts
@@ -3,6 +3,7 @@ import { User } from '../../../typeorm/src/entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Video } from '../../../typeorm/src/entity/video.entity';
+import { UserDto, UserResponseDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -14,12 +15,21 @@ export class UserService {
     private readonly videoRepository: Repository<Video>,
   ) {}
 
-  public async findUser(id: number): Promise<User> {
-    return await this.userRepository.findOne({
-      where: {
-        id,
-      },
-    });
+  private createUserResponse(user: User): UserResponseDto {
+    const UserResponse: UserResponseDto = {
+      id: user.id,
+      username: user.username,
+      description: user.description,
+      avatar: user.avatar,
+      email: user.email,
+      githubId: user.githubId,
+    };
+    return UserResponse;
+  }
+
+  public async findUser(id: number): Promise<UserResponseDto> {
+    const user = await this.userRepository.findOne(id);
+    return this.createUserResponse(user);
   }
 
   public async findVideos(userId: number): Promise<Video[]> {
@@ -30,5 +40,24 @@ export class UserService {
         },
       },
     });
+  }
+
+  public async insertUser(createUserDto: UserDto): Promise<UserResponseDto> {
+    const user = this.userRepository.create(createUserDto);
+    const result = await this.userRepository.insert(user);
+    return this.createUserResponse(user);
+  }
+
+  public async deleteUser(id): Promise<UserResponseDto> {
+    const user = await this.userRepository.findOne(id);
+    const result = await this.userRepository.delete(user);
+    return this.createUserResponse(user);
+  }
+
+  public async putUser(id, putUserDto: UserDto): Promise<UserResponseDto> {
+    const user = await this.userRepository.findOne(id);
+    this.userRepository.merge(user, putUserDto);
+    const result = await this.userRepository.save(user);
+    return this.createUserResponse(user);
   }
 }
