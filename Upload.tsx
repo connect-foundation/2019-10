39d026a9eaab08c7cf4f394b5332b1c96139f5d4
Atercatus <myleelee2053@naver.com>
2019-11-22 12:16:43 +0900

    [review] AWS Lambda 와 React video upload component
    클라이언트에서 S3 로 접근할 수 있는 pre-signed-url을 반환해주는 람다와 Transcoder 에 Job을 생성하는 람다의 코드입니다.
    Upload.tsx 는 클라이언트에서 비디오를 업로드 할 수 있게 하는 컴포넌트 입니다.

---
+++ Upload.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import uuidv4 from 'uuid/v4';
+import { UploadedVideoInfo } from '../dto/upload-video.dto';
+
+const VideoUpload: React.FunctionComponent = () => {
+  const fileInput = React.createRef<HTMLInputElement>();
+
+  const uploadVideo = async e => {
+    e.preventDefault();
+
+    const file = fileInput.current.files[0];
+    const id = uuidv4();
+    const fileName = `${id}/${file.name}`;
+
+    const preSignedUrl = await getPreSignedUrl(fileName);
+
+    if (!preSignedUrl) {
+      return;
+    }
+
+    const isUploadSuccess = await uploadToBucket(preSignedUrl, file);
+
+    if (!isUploadSuccess) {
+      return;
+    }
+
+    const video = await sendVideoInfo(
+      new UploadedVideoInfo(id, 'title', 'description', 0, ['tag1', 'tag2']),
+    );
+
+    if (!video) {
+      return;
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={uploadVideo}>
+        <input type="file" ref={fileInput} />
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+const getPreSignedUrl = async (fileName): Promise<string> => {
+  try {
+    const option = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ fileName: `workspace/${fileName}` }),
+    };
+
+    const response = await fetch(process.env.AUTH_LAMBDA_HOST, option);
+    const url = await response.json();
+
+    return url;
+  } catch (err) {
+    // TODO: 예외처리
+    return null;
+  }
+};
+
+const uploadToBucket = async (preSignedUrl, file): Promise<boolean> => {
+  try {
+    const option = {
+      method: 'PUT',
+      body: file,
+    };
+
+    const response = await fetch(preSignedUrl, option);
+
+    return response.ok;
+  } catch (err) {
+    // TODO: 예외처리
+    return null;
+  }
+};
+
+const sendVideoInfo = async (videoInfo: UploadedVideoInfo) => {
+  try {
+    const option = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(videoInfo),
+    };
+
+    const response = await fetch(process.env.API_SERVER_URL, option);
+    const video = await response.json();
+
+    return video;
+  } catch (err) {
+    // TODO: 예외 처리
+    return null;
+  }
+};
+
+export default VideoUpload;
