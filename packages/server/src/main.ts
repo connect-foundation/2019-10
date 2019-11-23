cbae456bc518f495c0cbbb8ad7263220c50c93b9
Alex Kwon <khwalex@gmail.com>
2019-11-23 19:17:24 +0900

    [server] | feat(cors): cors옵션 설정 및 허용 origin을 .env에 추가

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ALLOWED_ORIGINS.split(','),
+  });
   await app.listen(4000);
 }
 bootstrap();
