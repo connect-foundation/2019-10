29e057a4df9ea16b6e0ac838399969a8acaa3f4b
Alex Kwon <khwalex@gmail.com>
2019-11-23 18:53:14 +0900

    [client] | refactor(SearchBar): 검색 창 스타일 수정

--- packages/client/components/SearchBar/styles.tsx
+++ packages/client/components/SearchBar/styles.tsx
@@ -16,24 +16,41 @@ export const Input = styled.div`
   height: 3.2rem;
   background-color: #484c50;
   border-radius: 0.5rem;
-  padding-left: 1rem;
   align-items: center;
+  position: relative;
   display: ${props => (props.isActive ? 'flex' : 'none')};
+  padding-left: 4rem;
+  padding-right: 2rem;
+
   @media only screen and (min-width: ${BREAKPOINT}px) {
-    display: flex;
-    width: 28rem;
+    display: block;
+    max-width: 28rem;
+    flex: 1;
+    padding-left: 4.3rem;
+    padding-right: 2rem;
   }
+
   svg {
+    position: absolute;
+    left: 1rem;
+    top: calc(50% - 10px);
     vertical-align: middle;
+
+    @media only screen and (min-width: ${BREAKPOINT}px) {
+      left: 1.3rem;
+    }
   }
+
   input {
+    border-radius: 0.5rem;
     width: 100%;
+    height: 100%;
     background-color: #484c50;
     vertical-align: middle;
     font-size: 1.6rem;
-    margin-left: 1rem;
     color: white;
     border: none;
     outline: none;
+    padding: 0;
   }
 `;
