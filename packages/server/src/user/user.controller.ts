eb2ba7ca5965e17496cc00fe91c3a36783679e62
riverandeye <riverandeye@gmail.com>
2019-11-23 15:23:16 +0900

    [server] | feat(user): pipe를 이용한 user api 정의
    각 api에 Pipe를 적용하여 입력을 validate 하였습니다.
    ValidateUserPipe는 현재 완성하진 못했고, 추후 업데이트해야 합니다.

--- packages/server/src/user/user.controller.ts
+++ packages/server/src/user/user.controller.ts
@@ -6,40 +6,50 @@ import {
   Delete,
   Body,
   Put,
+  UsePipes,
 } from '@nestjs/common';
-import { User } from '../../../typeorm/src/entity/user.entity';
 import { UserService } from './user.service';
 import { Video } from '../../../typeorm/src/entity/video.entity';
-import { UserDto, UserResponseDto } from './dto';
+import { UserDto, UserResponseDto, UserIdParamDto } from './dto';
+import { ValidateIdParamPipe } from '../common/pipes';
+import { ValidateUserPipe } from './pipes';
 
 @Controller('users')
 export class UserController {
   public constructor(private readonly userService: UserService) {}
-  @Get(':id')
-  public async getUser(@Param('id') id: number): Promise<UserResponseDto> {
+  @Get('/:id')
+  @UsePipes(ValidateIdParamPipe)
+  public async getUser(
+    @Param() { id }: UserIdParamDto,
+  ): Promise<UserResponseDto> {
     return await this.userService.findUser(id);
   }
 
-  @Get(':userId/videos')
-  public async getVideos(@Param('userId') userId: number): Promise<Video[]> {
-    return await this.userService.findVideos(userId);
+  @Get('/:id/videos')
+  @UsePipes(ValidateIdParamPipe)
+  public async getVideos(@Param() { id }: UserIdParamDto): Promise<Video[]> {
+    return await this.userService.findVideos(id);
   }
 
-  @Post('')
-  public async createUser(@Body() userDto: UserDto): Promise<UserResponseDto> {
-    return await this.userService.insertUser(userDto);
+  @Post('/')
+  @UsePipes(ValidateUserPipe)
+  public async createUser(
+    @Body() createUserDto: UserDto,
+  ): Promise<UserResponseDto> {
+    return await this.userService.insertUser(createUserDto);
   }
 
-  @Delete(':id')
-  public async deleteUser(@Param('id') id: number): Promise<UserResponseDto> {
+  @Delete('/:id')
+  @UsePipes(ValidateIdParamPipe)
+  public async deleteUser(
+    @Param() { id }: UserIdParamDto,
+  ): Promise<UserResponseDto> {
     return await this.userService.deleteUser(id);
   }
 
-  @Put(':id')
-  public async updateUser(
-    @Param('id') id,
-    @Body() userDto: UserDto,
-  ): Promise<UserResponseDto> {
-    return await this.userService.putUser(id, userDto);
+  @Put('/')
+  @UsePipes(ValidateUserPipe)
+  public async updateUser(@Body() userDto: UserDto): Promise<UserResponseDto> {
+    return await this.userService.putUser(userDto);
   }
 }
