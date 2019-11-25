c3ff46054f44c2e98c5cd729923258f83808424a
Atercatus <myleelee2053@naver.com>
2019-11-25 16:48:54 +0900

    [client] | feat(transcoder-webhook): Transcoder의 작업 상태에 따른 동작 구현
    aws sns를 이용하여 Transcoder의 작업 경과 메시지를 받아 그에 따른 동작을 구현했습니다.
    우선 'COMPLETED'일 경우에 table로 유지된 비디오 정보를 가지고와서 DB에 저장하는 로직을 구현했습니다.

--- packages/server/src/video/video.controller.ts
+++ packages/server/src/video/video.controller.ts
@@ -1,12 +1,88 @@
-import { Controller, Get, Query, UsePipes } from '@nestjs/common';
+import { Controller, Body, Get, Post, Query, UsePipes } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { Video } from '../../../typeorm/src/entity/video.entity';
 import { VideosQueryDto } from './dto/videos-query.dto';
 import { GetVideosPipe } from '../common/pipes/get-videos.pipe';
+import { UploadedVideoInfo } from '../webhook/transcoder/dto/uploaded-video-info.dto';
+import { UploadedVideoTableService } from 'src/uploaded-video/uploaded-video-table.service';
+import { Repository } from 'typeorm';
 
 @Controller('videos')
 export class VideoController {
-  public constructor(private readonly videoService: VideoService) {}
+  public constructor(
+    private readonly videoService: VideoService,
+    private readonly uploadedVideoTableService: UploadedVideoTableService,
+    private readonly videoRepository: Repository<Video>,
+  ) {}
+
+  // @Post('/')
+  // public async listenTranscoderNotification(
+  //   @Body() notificationMessage: TranscoderNotificationDto,
+  // ) {
+  // tslint:disable-next-line: max-line-length
+  //   if (notificationMessage.state === TranscoderNotificationState.completed) {
+  // tslint:disable-next-line: max-line-length
+  //     const mpdSourceUrl = `https://${process.env.BUCKET_NAME}.s3-${process.env.BUCKET_REGION}.amazonaws.com/${notificationMessage.outputKeyPrefix}${notificationMessage.playlists[0].name}`;
+
+  //     const path = notificationMessage.outputKeyPrefix.split('/');
+  //     const id = path[path.find.length - 2];
+  //     // console.log(id);
+
+  //     const videoInfo = this.uploadedVideoTableService.find(id);
+  //     videoInfo.sourceUrl = mpdSourceUrl;
+  //     // console.log(mpdSourceUrl);
+
+  //     // const user;
+
+  //     const tags = await videoInfo.tags.reduce(
+  //       // tslint:disable-next-line: no-any
+  //       async (promiseResult: Promise<number[]>, tagName: any) => {
+  //         const tag = await this.tagRepository.find({
+  //           where: { name: tagName },
+  //         })[0];
+
+  //         if (!tag) {
+  //           const newTag = this.tagRepository.create({ name: tagName });
+  //           newTag.name = tagName;
+  //           await this.tagRepository.save(newTag);
+
+  //           // tslint:disable-next-line: no-shadowed-variable
+  //           const result = await promiseResult;
+
+  //           // console.log(result);
+
+  //           return Promise.resolve([...result, newTag.id]);
+  //         }
+  //         const result = await promiseResult;
+  //         // console.log(result);
+
+  //         return Promise.resolve([...result, tag.id]);
+  //       },
+  //       Promise.resolve([]),
+  //     );
+
+  //     const video = this.videoRepository.create({
+  //       title: videoInfo.title,
+  //       description: videoInfo.description,
+  //       sourceUrl: videoInfo.sourceUrl,
+  //       // thumbnail:
+  //       playtime: notificationMessage.outputs[0].duration,
+  //       tags,
+  //     });
+  //     // console.log(videoInfo);
+  //     // console.log(video);
+
+  //     await this.videoRepository.save(video);
+  //   }
+  // }
+
+  @Post('upload')
+  public saveVideoInfo(@Body() uploadedVideoInfo: UploadedVideoInfo) {
+    return this.uploadedVideoTableService.insert(
+      uploadedVideoInfo.id,
+      uploadedVideoInfo,
+    );
+  }
 
   @Get('/')
   @UsePipes(GetVideosPipe)
--- packages/server/src/video/video.module.ts
+++ packages/server/src/video/video.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoController } from './video.controller';
 import { VideoService } from './video.service';
 import { Video } from '../../../typeorm/src/entity/video.entity';
+import { UploadedVideoTableModule } from 'src/uploaded-video/uploaded-video-table.module';
+import { Tag } from '../../../typeorm/src/entity/tag.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Video])],
-  controllers: [VideoController],
+  imports: [TypeOrmModule.forFeature([Video, Tag]), UploadedVideoTableModule],
   providers: [VideoService],
+  controllers: [VideoController],
 })
 export class VideoModule {}
