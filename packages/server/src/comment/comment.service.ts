a585079f81980849c11487e18ee8476b2eb48040
Alex Kwon <khwalex@gmail.com>
2019-11-26 15:26:43 +0900

    [server] | feat(comments): 댓글 관련 로직 추가
    comment service에서 댓글 데이터를 반환하는 로직을 작성했습니다.
    
    - findComment 메소드를 통해 한 개의 댓글 항목을 반환할 수 있습니다.
    - findCommentsByVideo 메소드를 통해 영상 항목에 작성된 댓글 항목 리스트를 반환할 수 있습니다.
    - findReplies메소드를 통해 한 개의 댓글 항목에 잓어된 대댓글 항목 리스트를 반환할 수 있습니다.

--- packages/server/src/comment/comment.service.ts
+++ packages/server/src/comment/comment.service.ts
@@ -1,4 +1,80 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Comment } from '../../../typeorm/src/entity/comment.entity';
+
+import { POPULAR } from '../video/constants';
+import {
+  COMMENT_ITEMS_PER_PAGE,
+  COMMENT_QUERY_SELECT_COLUMNS,
+} from './constants';
+import { getOffset } from '../common/utils/get-offset';
+
 @Injectable()
 export class CommentService {
+  public constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>,
+  ) {}
+
+  public async findComment(id) {
+    return await this.commentRepository
+      .createQueryBuilder()
+      .select('Comment')
+      .where({ id })
+      .getOne();
+  }
+
+  public async findCommentsByVideo({
+    videoId,
+    page,
+    sort,
+  }): Promise<Comment[]> {
+    const offset = getOffset(page, COMMENT_ITEMS_PER_PAGE);
+
+    const qb = this.commentRepository
+      .createQueryBuilder()
+      .leftJoin('Comment.user', 'User')
+      .select(COMMENT_QUERY_SELECT_COLUMNS)
+      .addSelect(['User.id', 'User.username', 'User.avatar'])
+      .limit(COMMENT_ITEMS_PER_PAGE)
+      .offset(offset)
+      .where({
+        parent: {
+          id: null,
+        },
+        video: {
+          id: videoId,
+        },
+      });
+
+    if (sort === POPULAR) {
+      return qb.orderBy('Comment_popularity', 'DESC').getMany();
+    }
+
+    return qb.orderBy('Comment_createdAt', 'DESC').getMany();
+  }
+
+  public async findReplies({ id, videoId, page }): Promise<Comment[]> {
+    const offset = getOffset(page, COMMENT_ITEMS_PER_PAGE);
+
+    return await this.commentRepository
+      .createQueryBuilder()
+      .leftJoin('Comment.user', 'User')
+      .select(COMMENT_QUERY_SELECT_COLUMNS)
+      .addSelect(['User.id', 'User.username', 'User.avatar'])
+      .limit(COMMENT_ITEMS_PER_PAGE)
+      .offset(offset)
+      .where({
+        parent: {
+          id,
+        },
+        video: {
+          id: videoId,
+        },
+      })
+      .orderBy('Comment_createdAt', 'ASC')
+      .getMany();
+  }
 }
---
+++ packages/server/src/comment/constants.ts
@@ -0,0 +1,10 @@
+export const COMMENT_ITEMS_PER_PAGE = 10;
+
+export const COMMENT_QUERY_SELECT_COLUMNS = [
+  'Comment.id',
+  'Comment.content',
+  'Comment.likedUsersCount',
+  'Comment.popularity',
+  'Comment.createdAt',
+  'Comment.updatedAt',
+];
