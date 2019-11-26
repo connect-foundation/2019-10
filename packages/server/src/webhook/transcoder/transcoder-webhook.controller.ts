f6a2115fd5ff4ce83bdad8a919087801716d31dd
Atercatus <myleelee2053@naver.com>
2019-11-27 01:39:54 +0900

    [server & client] | refactor: .env.dummy 항목 추가

--- packages/server/src/webhook/transcoder/transcoder-webhook.controller.ts
+++ packages/server/src/webhook/transcoder/transcoder-webhook.controller.ts
@@ -33,7 +33,7 @@ export class TranscoderWebhookController {
       const videoInfo = this.uploadedVideoTableService.find(id);
       videoInfo.sourceUrl = mpdSourceUrl;
 
-      const user = await this.userRepository.find({ where: { id: 0 } });
+      const user = await this.userRepository.find({ where: { id: 1 } });
 
       const tags = await videoInfo.tags.reduce(
         // tslint:disable-next-line: no-any
