c0e325d9ca37746396bc7ab5a6f3fc6d7d4e111b
Atercatus <myleelee2053@naver.com>
2019-11-27 17:56:48 +0900

    [server] | feat(constants): 상수 선언
    클라이언트 페이지를 반환하는 클라이언트 서버의 경로를 변수로 지정했습니다.
    쿠키의 만료기간을 설정하기 위해 하루를 밀리세컨드로 변환한 값을 가지는 변수를 선언했습니다.

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -7,4 +7,12 @@ export const TranscoderNotificationState = {
 
 export const endpoint = {
   auth: 'auth',
+  githubLogin: '/github/login',
 };
+
+export const clientPath = {
+  signUp: 'http://localhost:3000/auth/signup',
+  main: 'http://localhost:3000',
+};
+
+export const ONE_DAY = 24 * 3600 * 1000;
