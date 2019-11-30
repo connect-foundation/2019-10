74d18e20bd018dd7763025406bd138b111b3c707
Atercatus <myleelee2053@naver.com>
2019-11-30 21:29:36 +0900

    [server] | feat(cors): client에서 온 request 내에 cookie가 설정 될 수 있게 수정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,15 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CORS_ALLOWED_ORIGINS.split(','),
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   app.use(helmet());
   app.use(bodyParser.text());
+  app.use(cookieParser());
 
   await app.listen(4000);
 }
