ebfb35d27f95ac69ad856901e9f42927d3f1b9c8
Atercatus <myleelee2053@naver.com>
2019-11-30 21:32:26 +0900

    [client] : feat(SignUp): 회원가입 기능 추가
    회원가입 시 서버에게 송신되는 FormData를 클래스로 만들어서 다른 함수에서 타입으로 사용할 수 있게 했습니다.

--- packages/client/views/SignUp/index.tsx
+++ packages/client/views/SignUp/index.tsx
@@ -1,28 +1,52 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
 import * as S from './styles';
-import { maxLength } from '../../constants';
+import { signUpFormDataMaxLength, routePath } from '../../constants';
+import { responseStatus } from '../../response';
+
+class FormData {
+  public constructor(
+    public readonly username: string,
+    public readonly description: string,
+    public readonly isAgreed: boolean,
+  ) {}
+}
 
 const SignUp: React.FunctionComponent = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    description: '',
-    isAgreed: false,
-  });
+  const [formData, setFormData] = useState(new FormData('', '', false));
 
   const [isFetching, setIsFetching] = useState(false);
+  const router = useRouter();
 
-  const handleChange = e => {
-    const value =
-      e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-    const name = e.target.name;
+  const handleFormChange = e => {
+    const { checked, name, value, type } = e.target;
+    const valueToUpdate = type === 'checkbox' ? checked : value;
+
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: valueToUpdate,
     });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     setIsFetching(true);
+
+    // 실제 요청 보내기
+    const response = await submitFormData(formData);
+
+    if (response.status === responseStatus.unprocessableEntity) {
+      setIsFetching(false);
+      window.alert('중복된 계정입니다');
+    }
+
+    if (response.status === responseStatus.unAuthorized) {
+      router.push(routePath.login);
+    }
+
+    if (response.ok) {
+      router.push(routePath.hotlist);
+    }
   };
 
   const checkFormVerified = () => {
@@ -49,8 +73,8 @@ const SignUp: React.FunctionComponent = () => {
             <input
               id="username"
               name="username"
-              maxLength={maxLength.username}
-              onChange={handleChange}
+              maxLength={signUpFormDataMaxLength.username}
+              onChange={handleFormChange}
               type="text"
             />
           </S.Item>
@@ -62,13 +86,17 @@ const SignUp: React.FunctionComponent = () => {
             <textarea
               id="description"
               name="description"
-              maxLength={maxLength.introduction}
-              onChange={handleChange}
+              maxLength={signUpFormDataMaxLength.introduction}
+              onChange={handleFormChange}
             />
           </S.Item>
           <S.Item>
             <S.Label>
-              <input type="checkbox" name="isAgreed" onChange={handleChange} />
+              <input
+                type="checkbox"
+                name="isAgreed"
+                onChange={handleFormChange}
+              />
               <div className={'agreement'}>
                 <u>서비스 약관</u>에 동의합니다.
               </div>
@@ -83,6 +111,19 @@ const SignUp: React.FunctionComponent = () => {
       </S.Container>
     </S.SignUp>
   );
+};
+
+const submitFormData = async (formData: FormData): Promise<Response> => {
+  const response = await fetch(process.env.SIGN_UP_API_SERVER_URL, {
+    body: JSON.stringify({ ...formData }),
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response;
 };
 
 export default SignUp;
