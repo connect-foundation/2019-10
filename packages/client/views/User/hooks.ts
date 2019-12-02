d45729c743796495341a7b3688a1bfafdb084be2
Alex Kwon <khwalex@gmail.com>
2019-12-03 07:56:17 +0900

    [client] | feat(User): 사용자 비디오 리스트 호출 hook 작성

--- packages/client/views/User/hooks.ts
+++ packages/client/views/User/hooks.ts
@@ -33,3 +33,45 @@ export const useUser = id => {
     ...rest,
   };
 };
+
+const createVideosAction: Action = (id, page, sort) => ({
+  method: 'GET',
+  endpoint: `${process.env.API_URL_HOST}/users/${id}/videos?page=${page}&sort=${sort}`,
+});
+
+export const useVideos = id => {
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState('popular'); // popular or latest
+  const [count, setCount] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const [hasData, setHasData] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const action = createVideosAction(id, page, sort);
+  const { payload, error, ...rest } = useQuery(action);
+
+  useEffect(() => {
+    setPage(1);
+    setHasData(false);
+  }, [sort]);
+
+  useEffect(() => {
+    if (payload && !error) {
+      setHasData(true);
+      setHasMore(payload.data.length >= 12);
+      setCount(payload.count);
+      page === 1
+        ? setVideos(payload.data)
+        : setVideos([...videos, ...payload.data]);
+    }
+  }, [payload, error]);
+
+  return {
+    videos,
+    count,
+    sort,
+    hasMore,
+    hasData,
+    onNext: () => setPage(page + 1),
+    onSort: value => setSort(value),
+  };
+};
