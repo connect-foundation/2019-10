71213cb0c2a5324702b2774c7446ca4768db7f62
Alex Kwon <khwalex@gmail.com>
2019-12-03 07:54:02 +0900

    [client] | feat(UserInfo): 유저 상세 데이터 렌더링 및 스켈레톤 작성

--- packages/client/components/UserProfile/index.tsx
+++ packages/client/components/UserProfile/index.tsx
@@ -3,26 +3,20 @@ import React from 'react';
 import * as S from './styles';
 import { Grid } from '@material-ui/core';
 
-const UserProfile = () => {
-  return (
+import UserProfileSkeleton from './skeleton';
+
+const UserProfile = ({ skeleton, avatar, username, description }) => {
+  return skeleton ? (
+    <UserProfileSkeleton />
+  ) : (
     <S.UserProfile>
       <S.Content>
         <S.Avatar>
-          <img src="https://miro.medium.com/max/3150/1*n4VB9UbNNoB78-vGIhulag.jpeg" />
+          <img src={avatar} />
         </S.Avatar>
         <S.Info>
-          <h1>Alex Kwon</h1>
-          <div>
-            안녕하세요?
-            <br />
-            개발하면서 이것저것 하는 한량이에요.
-            <br />
-            <br />
-            GCP를 좋아하고, pixel perfect 한 프론트엔드 앱을 좋아해요.
-            <br />
-            소프트웨어는 사용자에 초점을 맞춰야해요. 그 외는 전부 부수적인 것
-            뿐이죠.
-          </div>
+          <h1>{username}</h1>
+          <div>{description}</div>
         </S.Info>
       </S.Content>
     </S.UserProfile>
