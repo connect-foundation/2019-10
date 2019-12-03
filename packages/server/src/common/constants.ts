4ef12fc797024659edc7a769425afe502d0866c2
Alex Kwon <khwalex@gmail.com>
2019-12-03 17:16:02 +0900

    [server] | refactor(): 토큰 만료 시간을 한시간에서 15분으로 변경

--- packages/server/src/common/constants.ts
+++ packages/server/src/common/constants.ts
@@ -19,8 +19,10 @@ export const clientPath = {
 export const SESSION_TOKEN = 'SessionToken';
 export const GITHUB_USER_DETAIL = 'GithubUserDetail';
 
+export const ONE_MINUTE_MILLISECONDS = 60 * 1000;
 export const ONE_DAY_MILLISECONDS = 24 * 3600 * 1000;
 export const ONE_HOUR__MILLISECONDS = 3600 * 1000;
 
+export const ONE_MINUTE_SECONDS = 60;
 export const ONE_DAY_SECONDS = 24 * 3600;
 export const ONE_HOUR_SECONDS = 3600;
