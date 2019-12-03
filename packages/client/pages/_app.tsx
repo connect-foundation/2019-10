f347e97f6cab397c633499be4b995a79ac4f1511
Alex Kwon <khwalex@gmail.com>
2019-12-03 17:25:13 +0900

    [client] | feat(_app): 토큰 인증 로직 추가
    클라이언트 앱이 서버사이드에서 렌더링 될 때, 요청 헤더의 쿠키에서 토큰을 추출한 후 토큰의 유효성을 검사합니다. 만약 유효한 토큰이라면, 토큰의 data에서 유저 정보를 추출한 후에 클라이언트사이드에 props에 담아 전달합니다.

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import App from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import 'isomorphic-unfetch';
+import jwt from 'jsonwebtoken';
 import {
   ClientContextProvider as FetchingProvider,
   QueryResponse,
 } from 'react-fetching-library';
+import Cookies from 'universal-cookie';
 
 import { client } from '../libs/fetching';
 
@@ -15,16 +17,51 @@ interface AppProps {
   cacheItems: {
     [key: string]: QueryResponse;
   };
+  user: User;
+}
+
+interface User {
+  id: number;
+  email: string;
+  username: string;
+  avatar: string;
+}
+
+interface Token {
+  data: {
+    sessionId: string;
+    userPublicInfo: User;
+  };
+  iat: number;
+  exp: number;
 }
 
 class MyApp extends App<AppProps> {
   public static async getInitialProps(appContext) {
     const appProps = await App.getInitialProps(appContext);
 
-    return {
+    const props = {
       ...appProps,
       cacheItems: client.cache.getItems(),
+      user: null,
     };
+
+    // only on server-side
+    if (appContext.ctx.req) {
+      const cookies = new Cookies(appContext.ctx.req.headers.cookie);
+      const token = cookies.get('SessionToken');
+
+      try {
+        const {
+          data: { userPublicInfo },
+        } = jwt.verify(token, 'HUNGRY') as Token;
+        props.user = userPublicInfo;
+      } catch (err) {
+        // console.log(err);
+      }
+    }
+
+    return props;
   }
 
   public render() {
