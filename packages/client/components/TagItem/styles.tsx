2e9f7be4a48a4c39956a8c9725634b60a4b552b8
riverandeye <riverandeye@gmail.com>
2019-12-04 14:12:51 +0900

    [client] | feature(TagItem): 인기 태그 페이지의 태그 컴포넌트 작성
    태그 UI를 분리하여 컴포넌트로 작성하였습니다.

---
+++ packages/client/components/TagItem/styles.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { fontWeight } from '../../constants';
+
+export const Tag = styled.div`
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #484c50;
+  border-radius: 0.5rem;
+`;
+
+export const TagTitle = styled.div`
+  font-size: 1.8rem;
+  font-weight: ${fontWeight.bold};
+  margin-bottom: 0.5rem;
+`;
+
+export const TagCount = styled.div`
+  font-size: 1.6rem;
+  font-weight: ${fontWeight.regular};
+`;
