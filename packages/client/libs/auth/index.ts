cc10607c0a22438f5e99581feb421476c2ee3801
Atercatus <myleelee2053@naver.com>
2019-12-07 20:40:36 +0900

    [client] | feat(App): 페이지 마다 다르게 render 하기 위한 로직 작성
    비디오 업로드 시 발생하는 Context를 비디오 업로드 페이지에서만 제공하기 위한 로직과 서버사이드에서 로그인 되어 있는지 판별 할 수 있는 로직을 작성했습니다.

--- packages/client/libs/auth/index.ts
+++ packages/client/libs/auth/index.ts
@@ -1,5 +1,20 @@
 import { responseStatus } from '../../response';
 import { ServerResponse } from 'http';
+import { useRouter } from 'next/router';
+import { useUser } from '../../components/UserProvider/hooks';
+import { endpoint } from '../../constants';
+
+/**
+ * @description: Only use it in hooks
+ */
+export const onlyMember = () => {
+  const router = useRouter();
+  const user = useUser();
+
+  if (checkLogInStatusClientSide(user)) {
+    router.push(endpoint.hotlist);
+  }
+};
 
 export const checkLogInStatusClientSide = user => {
   return !user;
