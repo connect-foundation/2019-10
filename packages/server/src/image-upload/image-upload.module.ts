fcb0205834d0cdb000cbeb093cf04a6fff33deef
riverandeye <riverandeye@gmail.com>
2019-12-09 03:36:03 +0900

    Revert "[server] | feat(ImageUpload): 이미지 업로드 모듈 작성"
    This reverts commit 84abdb6b69dd583d8b55ac1add1d5f64b4d26b07.

--- packages/server/src/image-upload/image-upload.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ImageUploadService } from 'image-upload/image-upload.service';
-
-@Module({
-  imports: [],
-  providers: [ImageUploadService],
-  exports: [ImageUploadService],
-})
-export class ImageUploadModule {}
