006b3b74637482bbdd2ae2ff861b67d0ce2c29ff
riverandeye <riverandeye@gmail.com>
2019-12-09 20:54:19 +0900

    [server] | feat(Tag): 태그 비디오 목록을 반환하는 DTO 작성
    태그 비디오 목록을 반환하는 DTO를 작성하였습니다.

---
+++ packages/server/src/tag/dto/tag-video-list-response.dto.ts
@@ -0,0 +1,11 @@
+import { Video } from '../../../../typeorm/src/entity/video.entity';
+import { VideoInfo } from 'video/model/video-info';
+
+export class TagVideoListResponseDto {
+  public constructor(videoList: Video[], count: number) {
+    this.data = videoList.map(video => new VideoInfo(video));
+    this.count = count;
+  }
+  public readonly data: VideoInfo[];
+  public readonly count: number;
+}
