a3500078cbe946da41a793ea14dabbef70a1ad84
kimjihye <eoeogudgud@naver.com>
2019-12-09 17:21:48 +0900

    [client] | feat(depoly): 클라이언트 배포
    배포시 now --prod 로 해주시면 됩니다.

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,9 +2,13 @@ const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
+  target: 'serverless',
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
     return config;
+  },
+  env: {
+    API_URL_HOST: process.env.API_URL_HOST,
   },
 };
