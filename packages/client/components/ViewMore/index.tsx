7440fdc6787682f8a8654417a9c2a3c111976ad5
kimjihye <eoeogudgud@naver.com>
2019-12-13 15:36:31 +0900

    [client] | refactor(veiwMore): 변수이름 변경

--- packages/client/components/ViewMore/index.tsx
+++ packages/client/components/ViewMore/index.tsx
@@ -5,19 +5,19 @@ import * as S from './styles';
 import { ArrowRightSVG } from '../../svgs';
 import { endpoint, searchOptions } from '../../constants';
 
-const ViewMore = ({ searchKeyword, num, options }) => {
+const ViewMore = ({ searchKeyword, index, options }) => {
   return (
     <S.ViewMore>
       <Link
         prefetch={false}
         href={{
-          pathname: `${endpoint.search}/${searchOptions[num].value}`,
+          pathname: `${endpoint.search}/${searchOptions[index].value}`,
           query: { keyword: searchKeyword, options },
         }}
       >
         <a>
           <button>
-            <span>전체 {searchOptions[num].label}</span>
+            <span>전체 {searchOptions[index].label}</span>
             <ArrowRightSVG />
           </button>
         </a>
