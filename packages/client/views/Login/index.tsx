3aab13f49d692465a5a5891bf5daa65c3c75ce01
riverandeye <riverandeye@gmail.com>
2019-12-14 13:42:37 +0900

    [client] | refactor(Login): 컴포넌트 위치 변경 및 Fragment 제거
    불필요한 Fragment를 제거합니다.

--- packages/client/views/Login/index.tsx
+++ packages/client/views/Login/index.tsx
@@ -7,37 +7,35 @@ import { GitHubSVG } from '../../svgs';
 
 const Login: React.FunctionComponent = () => {
   return (
-    <>
-      <AppBar showSearchBar={false} showButtons={false} />
-      <S.Login>
-        <S.Container>
-          <S.ContainerGrid container justify="center">
-            <Grid item xs={12} md={8}>
-              <S.Title>
-                개발자를 위한
-                <br /> 동영상 커뮤니티.
-              </S.Title>
-              <S.Message>
-                위데브에 오신것을 환영합니다! <br />
-                여러분의 소중한 개발 경험을 공유하고, 양질의 개발 영상을
-                시청하세요!
-              </S.Message>
-              <S.Link>
-                <S.AuthenticateLink
-                  href={
-                    process.env.GITHUB_REQUEST_USER_IDENTITY_URL +
-                    `?client_id=${process.env.GITHUB_CLIENT_ID}`
-                  }
-                >
-                  <GitHubSVG />
-                  <span>GitHub 계정으로 로그인/회원가입</span>
-                </S.AuthenticateLink>
-              </S.Link>
-            </Grid>
-          </S.ContainerGrid>
-        </S.Container>
-      </S.Login>
-    </>
+    <S.Login>
+      <AppBar searchBar={false} buttons={false} backgroundColor="#303537" />
+      <S.Container>
+        <S.ContainerGrid container justify="center">
+          <Grid item xs={12} md={8}>
+            <S.Title>
+              개발자를 위한
+              <br /> 동영상 커뮤니티.
+            </S.Title>
+            <S.Message>
+              위데브에 오신것을 환영합니다! <br />
+              여러분의 소중한 개발 경험을 공유하고, 양질의 개발 영상을
+              시청하세요!
+            </S.Message>
+            <S.Link>
+              <S.AuthenticateLink
+                href={
+                  process.env.GITHUB_REQUEST_USER_IDENTITY_URL +
+                  `?client_id=${process.env.GITHUB_CLIENT_ID}`
+                }
+              >
+                <GitHubSVG />
+                <span>GitHub 계정으로 로그인/회원가입</span>
+              </S.AuthenticateLink>
+            </S.Link>
+          </Grid>
+        </S.ContainerGrid>
+      </S.Container>
+    </S.Login>
   );
 };
 
