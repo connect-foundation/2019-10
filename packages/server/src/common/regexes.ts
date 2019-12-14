fb2576a9c176f904941c19d4f8b97be929f1eebe
riverandeye <riverandeye@gmail.com>
2019-12-14 13:30:16 +0900

    [server] | fix(regex): Regex의 g modifier 제거
    state를 저장하여 예상치 못한 결과를 반환하게 하는 g modifier를 제거합니다.

--- packages/server/src/common/regexes.ts
+++ packages/server/src/common/regexes.ts
@@ -1,2 +1,2 @@
 export const NATURAL_NUMBER_REGEX = /^[1-9]\d*$/;
-export const USER_NAME_REGEX = /^[\s0-9a-zA-Z\u3131-\uD79D]{1,30}$/giu;
+export const USER_NAME_REGEX = /^[\s0-9a-zA-Z\u3131-\uD79D]{1,30}$/iu;
