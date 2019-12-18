56ebe635f439c32ee550b888e35842603245c13b
riverandeye <riverandeye@gmail.com>
2019-12-18 18:01:19 +0900

    [client] | refactor(TagVideos): 태그 가져오는 훅 내의 변수명 조정
    모호한 action이라는 변수명을 getTagAction 으로 변경하였습니다.

--- packages/client/views/TagVideos/hook/use-tag.ts
+++ packages/client/views/TagVideos/hook/use-tag.ts
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from 'react-fetching-library';
+
 import { Tag } from '../model/tag';
 import { makeQueryTagAction } from '../action/make-query-tag-action';
 
 export const useTag = (id: number) => {
   const [tag, setTag] = useState(new Tag(0, '', 0));
 
-  const action = makeQueryTagAction(id);
-  const { payload, error, ...rest } = useQuery(action);
+  const getTagAction = makeQueryTagAction(id);
+  const { payload, error } = useQuery(getTagAction);
 
   useEffect(() => {
     if (!payload) {
