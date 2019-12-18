351c6f896634fab80e2c3bcc5cca439379eebff3
riverandeye <riverandeye@gmail.com>
2019-12-18 15:07:45 +0900

    [client] | refactor(SignUp): username에 해당하는 사용자 찾는 액션 만드는 함수 이름 변경

--- packages/client/views/SignUp/action/make-get-user.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from 'react-fetching-library';
-
-import { endpoint } from '../../../constants';
-
-export const makeGetUserAction: Action = (username: string) => ({
-  method: 'GET',
-  endpoint: `${process.env.API_SERVER_URL}${endpoint.verifyUsername}/${username}`,
-});
