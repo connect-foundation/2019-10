47bd9ab4add57c6422c893dfbe81593596ab1c84
riverandeye <riverandeye@gmail.com>
2019-12-18 15:08:30 +0900

    [client] | fix(libs): 회원가입 요청에 생략된 isAgreed 정보 추가

--- packages/client/views/SignUp/dto/sign-up-form-dto-factory.ts
+++ packages/client/views/SignUp/dto/sign-up-form-dto-factory.ts
@@ -4,10 +4,12 @@ export class SignUpFormDTOFactory {
   public static makeSignUpFormDTO(
     username: string,
     description: string,
+    isAgreed: boolean,
   ): SignUpFormDTO {
     return {
       username,
       description,
+      isAgreed,
     };
   }
 }
--- packages/client/views/SignUp/dto/sign-up-form-dto.ts
+++ packages/client/views/SignUp/dto/sign-up-form-dto.ts
@@ -1,4 +1,5 @@
 export interface SignUpFormDTO {
   username: string;
   description: string;
+  isAgreed: boolean;
 }
