98126859cd2cffb86861bdaf2f489fc429c20f5f
riverandeye <riverandeye@gmail.com>
2019-12-19 15:10:40 +0900

    [client] | refactor(Profile): 불필요한 컴포넌트 삭제

--- packages/client/views/Profile/index.tsx
+++ packages/client/views/Profile/index.tsx
@@ -19,77 +19,75 @@ const Profile: React.FunctionComponent = () => {
 
   return (
     <Layout drawer={false}>
-      <S.Profile>
-        <S.ContainerGrid container justify="center">
-          <Grid item xs={12} md={6}>
-            <S.Title>
-              <SettingsSVG />
-              <span>프로필 편집</span>
-            </S.Title>
-            <S.Label>
-              <label>
-                프로필 사진
-                <S.RequireMark />
-              </label>
-            </S.Label>
-            <S.AvatarItem>
-              {userProfile.avatar ? (
-                <S.Avatar src={userProfile.avatar} />
-              ) : (
-                <S.AvatarSkeleton variant="circle" />
-              )}
-              <label htmlFor="avatar">
-                <S.AvatarInputLabel>
-                  {isAvatarFetching ? <S.AvatarCircularProgress /> : `변경하기`}
-                </S.AvatarInputLabel>
-              </label>
-              <input
-                id="avatar"
-                type="file"
-                accept="image/png, image/jpeg"
-                onChange={handleAvatarSubmit}
+      <S.ContainerGrid container justify="center">
+        <Grid item xs={12} md={6}>
+          <S.Title>
+            <SettingsSVG />
+            <span>프로필 편집</span>
+          </S.Title>
+          <S.Label>
+            <label>
+              프로필 사진
+              <S.RequireMark />
+            </label>
+          </S.Label>
+          <S.AvatarItem>
+            {userProfile.avatar ? (
+              <S.Avatar src={userProfile.avatar} />
+            ) : (
+              <S.AvatarSkeleton variant="circle" />
+            )}
+            <label htmlFor="avatar">
+              <S.AvatarInputLabel>
+                {isAvatarFetching ? <S.AvatarCircularProgress /> : `변경하기`}
+              </S.AvatarInputLabel>
+            </label>
+            <input
+              id="avatar"
+              type="file"
+              accept="image/png, image/jpeg"
+              onChange={handleAvatarSubmit}
+            />
+          </S.AvatarItem>
+          <S.Form>
+            <S.Item>
+              <S.Label>
+                <label>
+                  닉네임
+                  <S.RequireMark />
+                </label>
+                <span>(영문, 숫자 또는 한글)</span>
+              </S.Label>
+              <S.UserNameInput
+                id="username"
+                name="username"
+                type="text"
+                value={userProfile.username}
+                onChange={handleUsername}
+                spellCheck={false}
               />
-            </S.AvatarItem>
-            <S.Form>
-              <S.Item>
-                <S.Label>
-                  <label>
-                    닉네임
-                    <S.RequireMark />
-                  </label>
-                  <span>(영문, 숫자 또는 한글)</span>
-                </S.Label>
-                <S.UserNameInput
-                  id="username"
-                  name="username"
-                  type="text"
-                  value={userProfile.username}
-                  onChange={handleUsername}
-                  spellCheck={false}
-                />
-              </S.Item>
-              <S.Item>
-                <S.Label>
-                  <label htmlFor="description">소개</label>
-                  <span>(최대 1,500자)</span>
-                </S.Label>
-                <S.DescriptionInput
-                  id="description"
-                  name="description"
-                  value={userProfile.description}
-                  onChange={handleDescription}
-                  spellCheck={false}
-                />
-              </S.Item>
-              <S.SubmitButton>
-                <button type="button" onClick={handleFormSubmit}>
-                  {isFormFetching ? <S.FormCircularProgress /> : '제출하기'}
-                </button>
-              </S.SubmitButton>
-            </S.Form>
-          </Grid>
-        </S.ContainerGrid>
-      </S.Profile>
+            </S.Item>
+            <S.Item>
+              <S.Label>
+                <label htmlFor="description">소개</label>
+                <span>(최대 1,500자)</span>
+              </S.Label>
+              <S.DescriptionInput
+                id="description"
+                name="description"
+                value={userProfile.description}
+                onChange={handleDescription}
+                spellCheck={false}
+              />
+            </S.Item>
+            <S.SubmitButton>
+              <button type="button" onClick={handleFormSubmit}>
+                {isFormFetching ? <S.FormCircularProgress /> : '제출하기'}
+              </button>
+            </S.SubmitButton>
+          </S.Form>
+        </Grid>
+      </S.ContainerGrid>
     </Layout>
   );
 };
