ee1563f7a104688e626763c69990dedb5c914ec0
Alex Kwon <khwalex@gmail.com>
2019-12-19 15:06:43 +0900

    [server] | refactor(deploy): 배포 설정 추가

--- packages/server/config/config.js
+++ packages/server/config/config.js
@@ -1,13 +1,41 @@
 module.exports.CONFIG = serverless => ({
-  dev: {
-    API_NAME: 'wedev-api-dev',
+  prod: {
+    API_NAME: 'wedev-api-prod',
     SECURITY_GROUP_ID: 'sg-00716c37241964512',
     PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
     PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
     PRIVATE_SUBNET_3: 'subnet-0ef00aa4fb1674c5b',
   },
-  prod: {
-    API_NAME: 'wedev-api-prod',
+  stage: {
+    API_NAME: 'wedev-api-stage',
+    SECURITY_GROUP_ID: 'sg-00716c37241964512',
+    PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
+    PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
+    PRIVATE_SUBNET_3: 'subnet-0ef00aa4fb1674c5b',
+  },
+  alex: {
+    API_NAME: 'wedev-api-alex',
+    SECURITY_GROUP_ID: 'sg-00716c37241964512',
+    PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
+    PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
+    PRIVATE_SUBNET_3: 'subnet-0ef00aa4fb1674c5b',
+  },
+  atercatus: {
+    API_NAME: 'wedev-api-atercatus',
+    SECURITY_GROUP_ID: 'sg-00716c37241964512',
+    PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
+    PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
+    PRIVATE_SUBNET_3: 'subnet-0ef00aa4fb1674c5b',
+  },
+  kimjye: {
+    API_NAME: 'wedev-api-kimjye',
+    SECURITY_GROUP_ID: 'sg-00716c37241964512',
+    PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
+    PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
+    PRIVATE_SUBNET_3: 'subnet-0ef00aa4fb1674c5b',
+  },
+  riverandeye: {
+    API_NAME: 'wedev-api-riverandeye',
     SECURITY_GROUP_ID: 'sg-00716c37241964512',
     PRIVATE_SUBNET_1: 'subnet-018f7fd8aa3ae2c39',
     PRIVATE_SUBNET_2: 'subnet-0479aca67cf6ca711',
