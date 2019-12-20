3e80f64fad3a68477df6889f6ee815fab874c586
riverandeye <riverandeye@gmail.com>
2019-12-20 13:40:46 +0900

    [client] | fix(Tags): 태그를 눌렀을 때 해당 태그 페이지로 이동되지 않는 오류 수정

--- packages/client/components/TagItem/index.tsx
+++ packages/client/components/TagItem/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
+
 import * as S from './styles';
 import { Tag } from './interface/tag';
+import { endpoint } from '../../constants';
 
 const TagItem = (tag: Tag) => {
   return (
-    <S.Tag>
-      <S.TagTitle>{tag.name}</S.TagTitle>
-      <S.TagCount>{tag.videosCount.toLocaleString()}</S.TagCount>
-    </S.Tag>
+    <Link prefetch={false} href={`${endpoint.tags}/${tag.id}`}>
+      <S.Tag>
+        <S.TagTitle>{tag.name}</S.TagTitle>
+        <S.TagCount>{tag.videosCount.toLocaleString()}</S.TagCount>
+      </S.Tag>
+    </Link>
   );
 };
 
